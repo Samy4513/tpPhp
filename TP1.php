62e1d5d70f006917dc98cdb5cd5d661c2d407669
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-13 11:45:59 +0200

    Exo 4 tp1

--- TP1.php
+++ TP1.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
-<title>HTML Tutorial</title>
-<body>
-
-<h1>This is a heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html>
 
 <?php
-    echo date('lFd,Y');
+    echo date('lFd,Y').'<br>';
+    $jour=date('lFd,Y');
+    echo $jour;
 ?>
