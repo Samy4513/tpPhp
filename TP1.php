e185281c6a0f9f1d39a945668ebb8a3decc7b6cc
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-13 11:55:51 +0200

    Exo 4 tp1

--- TP1.php
+++ TP1.php
@@ -1,10 +1,14 @@
 
 <?php
-    echo date('lFd,Y').'<br>';
-    $jour=date('lFd,Y');
-    echo $jour.'<br>'.'<br>';
-    $jour2=date('d/m/Y',strtotime('2020-04-01'));
-    echo $jour2.'<br>'.'<br>';
+//    echo date('lFd,Y').'<br>';
+//    $jour=date('lFd,Y');
+//    echo $jour.'<br>'.'<br>';
+//    $jour2=date('d/m/Y',strtotime('2020-04-01'));
+//    echo $jour2.'<br>'.'<br>';
+
+    $jour=date('d/m/Y',strtotime('2001-03-12'));
+    echo $jour.'<br>';
+    echo date('12,04,13,2001');
 
 
 ?>
