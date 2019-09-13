81ff8ace1301fc3c8bbb14b75debf3a7a502323a
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-13 11:44:16 +0200

    Exo 4 tp1

--- TP1.php
+++ TP1.php
@@ -1,27 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
 
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
 
-<?php
-    function start_page($title)
-    {
-        echo'<!DOCTYPE html> 
-             <html lang="fr">
-             <head>
-             <title>'.PHP_EOL.$title.'</title>
-             </head>
-             <body>'.PHP_EOL;
-    };
-
-    function end_page()
-    {
-      echo '  </body>
-              </html>';
-    }
-;?>
-
-<hr/><br/><strong>Test</strong><br/><hr/>
+</body>
+</html>
 
 <?php
-    start_page('Test');
-     end_page();
-;?>
-
+    echo date('lFd,Y');
+?>
