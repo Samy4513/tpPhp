9f8327371e316c041c81ae37b1accb22532b8538
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-13 11:10:13 +0200

    Exo 3 tp1

--- TP1.php
+++ TP1.php
@@ -9,16 +9,16 @@
 </html>
 
 <?php
-   echo'<strong>Voici mon premier programme PHP</strong><br/>'."\n";
-   echo'C\'est pas mal non?<br/>';
+    function start_page($title)
+    {
+        echo'<!DOCTYPE html> 
+             <html lang="fr">
+             <head>
+             <title>'.PHP_EOL.$title.'</title>
+             </head>
+             <body>'.PHP_EOL;
+    };
 
-   $var1=6;
-   $var2=1.3;
-   $var3='Variable3';
-   echo"$var1+$var2" . '<br>';
-   echo$var1+$var2.'<br>';
-   echo$var1+$var3.'<br>';
-
-
+    start_page('titre');
 
 ;?>
