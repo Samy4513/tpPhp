ac83ad0378e53831518729482f6bfae856619b56
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-25 14:39:17 +0200

    Exo 5 tp1

--- TP1.php
+++ TP1.php
@@ -9,9 +9,10 @@
 ?>
 <form action="calcul.php" method="post">
 
-    <input checked="checked"   type="radio"    name="op"     value="*"/> * <br/>;
-    <input type="radio"        name="op1"      value="+"/> + <br/>;
-    <input type="radio"        name="op2"      value="-"/> - <br/>;
-    <input type="radio"        name="op3"      value="/"/> / <br/>;
+    <input type="text" name="calcul" value="Entrez le calcul"><br>
+    <input checked="checked"   type="radio"    name="op"     value="*"/> * <br/>
+    <input type="radio"        name="op1"      value="+"/> + <br/>
+    <input type="radio"        name="op2"      value="-"/> - <br/>
+    <input type="radio"        name="op3"      value="/"/> / <br/>
     <input type="submit" value="Submit">
 </form>
