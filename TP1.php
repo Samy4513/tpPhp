ccfb97b23a63f3dc31c141ae5d0c4a70a8f6b542
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-25 14:33:59 +0200

    Exo 5 tp1

--- TP1.php
+++ TP1.php
@@ -6,10 +6,12 @@
 //    $jour2=date('d/m/Y',strtotime('2020-04-01'));
 //    echo $jour2.'<br>'.'<br>';
 
-
-<input checked="checked"   type="radio"    name="op"     value="*"/> *;
-<input type="radio"        name="op1"      value="+"/> + ;
-<input type="radio"        name="op2"      value="-"/> - ;
-<input type="radio"        name="op3"      value="/"/> / ;
-
 ?>
+<form action="calcul.php" method="post">
+
+    <input checked="checked"   type="radio"    name="op"     value="*"/> *;
+    <input type="radio"        name="op1"      value="+"/> + ;
+    <input type="radio"        name="op2"      value="-"/> - ;
+    <input type="radio"        name="op3"      value="/"/> / ;
+    <input type="submit" value="Submit">
+</form>
