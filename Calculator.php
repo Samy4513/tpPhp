53c5a96bf75af20b537ba548cdefec7e3a94fe35
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-25 15:16:29 +0200

    Exo 5 tp1

--- Calculator.php
+++ Calculator.php
@@ -1,11 +1,11 @@
 <form action="Calcul.php" method="post">
 
-    <input type="text" name="calcul" ><br>
+    <input type="text" name="op1" ><br>
     <input checked="checked"   type="radio"    name="op"     value="*"/> * <br/>
     <input type="radio"        name="op"      value="+"/> + <br/>
     <input type="radio"        name="op"      value="-"/> - <br/>
     <input type="radio"        name="op"      value="/"/> / <br/>
-    <input type="text" name="calcul2" ><br>
+    <input type="text" name="op2" ><br>
     <input type="submit" value="Submit">
 </form>
 
