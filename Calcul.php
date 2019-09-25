fbb0168dcddc0eed36d1c8b3660a283971c3531f
k18012618 <samy.kanboui@etu.univ-amu.fr>
2019-09-25 15:18:55 +0200

    Exo 5 tp1

--- Calcul.php
+++ Calcul.php
@@ -21,4 +21,16 @@ else
     echo '<br/><strong>opérateur ' . $op . ' non géré </strong>';
 }
 
+$operateurs='*+-/';
+
+for($cpt=0;$cpt<=3;++$cpt)
+{
+    echo'<input';
+    if($cpt==0)
+        {
+        echo'checked="checked"';
+        }
+    echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>'."\n";
+}
+
 ?>
